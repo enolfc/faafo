dd6fa6c2f8ee0d6017e9cd274c5179b22900e0ab
Enol Fernandez <enolfc@ifca.unican.es>
2015-11-06 15:25:47 +0100

    Improving deploy

--- contrib/faafo-wrapper.sh
+++ contrib/faafo-wrapper.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-URL_MESSAGING='amqp://guest:guest@localhost:5672/'
-URL_DATABASE='sqlite:////tmp/sqlite.db'
-URL_ENDPOINT='http://127.0.0.1'
-
 # use the rabbitmq linked in docker
-if [ "x$RABBITMQ_PORT_5672_TCP_PORT" != "x" ]; then
-    URL_MESSAGING="amqp://guest:guest@rabbitmq:${RABBITMQ_PORT_5672_TCP_PORT}/"
+if [ "x$TRANSPORT_URL" = "x" ]; then
+    if [ "x$RABBITMQ_PORT_5672_TCP_PORT" != "x" ]; then
+        TRANSPORT_URL="amqp://guest:guest@rabbitmq:${RABBITMQ_PORT_5672_TCP_PORT}/"
+    else
+        TRANSPORT_URL='amqp://guest:guest@localhost:5672/'
+    fi
 fi
 
-if [ x"$API_PORT" != "x" ]; then
-    URL_ENDPOINT="http://api:${API_PORT_80_TCP_PORT}"
+if [ "x$ENDPOINT_URL" = "x" ]; then
+    if [ x"$API_PORT" != "x" ]; then
+        ENDPOINT_URL="http://api:${API_PORT_80_TCP_PORT}"
+    else
+        ENDPOINT_URL='http://127.0.0.1'
+    fi
 fi
 
 
-sed -i -e "s#transport_url = .*#transport_url = $URL_MESSAGING#" /etc/faafo/faafo.conf
-sed -i -e "s#database_url = .*#database_url = $URL_DATABASE#" /etc/faafo/faafo.conf
-sed -i -e "s#endpoint_url = .*#endpoint_url = $URL_ENDPOINT#" /etc/faafo/faafo.conf
+sed -i -e "s#transport_url = .*#transport_url = $TRANSPORT_URL#" /etc/faafo/faafo.conf
+sed -i -e "s#database_url = .*#database_url = $DATABASE_URL#" /etc/faafo/faafo.conf
+sed -i -e "s#endpoint_url = .*#endpoint_url = $ENDPOINT_URL#" /etc/faafo/faafo.conf
 
 exec "$@"
